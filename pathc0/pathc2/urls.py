ff47b7f8060f44f3f0e7d7f08987f53b4b1125eb
Your Name <you@example.com>
2024-02-22 21:43:10 +0530

    Forth commit

--- pathc0/pathc2/urls.py
+++ pathc0/pathc2/urls.py
@@ -4,7 +4,7 @@ from pathc2.views import index, subscribe
 app_name = 'pathc2'
 
 urlpatterns = [
-    path("", index, name="pathc2"),          # Maps the root URL to the index view
-    path('pathc2/', index, name='pathc2'),   # Maps '/pathc2/' URL to the index view
-    path("subscribe/", subscribe, name="subscribe"),  # Maps '/subscribe/' URL to the subscribe view
+    path("", index, name="pathc2"),          
+    path('pathc2/', index, name='pathc2'),
+    path("subscribe/", subscribe, name="subscribe"), 
 ]
